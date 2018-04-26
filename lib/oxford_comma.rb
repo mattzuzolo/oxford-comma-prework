bc7e96604db176382f93160939835ba229c7ef65
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 01:12:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,16 @@
 def oxford_comma(array)
   
   if (array.length == 1)
+    array[0]
+  
+  elsif (array.length == 2)
+    array.join(", ")
+  
+  else 
+    array[array.length - 1] = "and #{array.last}"
+    array.join(", ")
+  end  
     
     
-    
-  array[array.length - 1] = "and #{array.last}"
-
-  array.join(", ")
+  
 end
