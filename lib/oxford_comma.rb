899e554593cef00b99022a009a5abd461db3c528
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-26 01:12:38 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  
+  if (array.length == 1)
+    array[0]
+  
+  elsif (array.length == 2)
+    array.join(" and ")
+  
+  else 
+    array[array.length - 1] = "and #{array.last}"
+    array.join(", ")
+  end  
+    
+    
+  
 end
